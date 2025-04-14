4c1e5a8a9e6f1c4bea3b91503225062581641626
Leandro Oscar Falcon <leandro.falcon823@gmail.com>
2025-04-14 18:04:26 +0200

    Add files via upload

---
+++ Nivel_2.sql
@@ -0,0 +1,32 @@
+-- Script para Identifica los cinco días que se generó la mayor cantidad de ingresos en la empresa por ventas
+SELECT DATE(timestamp) as Fecha, SUM(amount) as Total_ventas
+FROM transaction
+GROUP BY DATE(timestamp)
+ORDER BY Total_ventas DESC
+LIMIT 5;
+
+-- Script para obtener la media de ventas por país
+SELECT country, AVG(amount) as Promedio_vtas
+FROM transaction
+JOIN company ON transaction.company_id = company.id
+GROUP BY country
+ORDER BY Promedio_vtas DESC;
+
+-- Script para obtener la lista de todas las transacciones realizadas por empresas que están ubicadas en el mismo país que 'Non Institute'
+SELECT * 
+FROM transaction 
+JOIN company ON transaction.company_id = company.id
+	WHERE country = (SELECT country
+	FROM transactions.company
+	WHERE company_name = 'Non Institute');
+
+SELECT *
+FROM transaction 
+WHERE company_id IN (
+	SELECT id
+	FROM company
+	WHERE country = (
+		SELECT country 
+        FROM company 
+        WHERE company_name = 'Non Institute')
+        );
